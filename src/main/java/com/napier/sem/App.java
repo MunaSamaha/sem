403831792635ad51b0603f93418b4b44ad614b04
Munasamaha <40685078@live.napier.ac.uk>
2024-02-08 00:09:40 +0300

    Added Hello
    Commit Message

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,9 +16,9 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "MunaSamaha")
+        Document doc = new Document("name", "MKevin Sim")
                 .append("class", "Software Engineering Methods")
-                .append("year", "2024")
+                .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
